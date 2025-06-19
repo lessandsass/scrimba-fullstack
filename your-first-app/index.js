1474c666dffa4e9977f4db53024f0c2a0fff87fb
Vipu <lessandsass@gmail.com>
2025-06-19 15:57:04 +0530

    update

--- your-first-app/index.js
+++ your-first-app/index.js
@@ -79,4 +79,4 @@ const App = (props) => {
 };
 
 // CHALLENGE: add your own name and emoji to the website
-ReactDOM.render(<App name="Per" emoji="🎉"/>, document.getElementById("root"));
+ReactDOM.render(<App name="Per" emoji=""/>, document.getElementById("root"));
